e0843374a061fc335955bb2901294885aae04641
supuni2006 <supuni2006k@gmail.com>
2026-08-31 10:48:27 +0530

    5678

--- assets/js/main.js
+++ assets/js/main.js
@@ -177,7 +177,8 @@
         { name: "Project 01", photos: [10,11,12,13] },
         { name: "Project 02", photos: [14,16,17] },
         { name: "Project 03", photos: [46,47,48] },
-        { name: "Wellampitiya", photos: [43,44,45] }
+        { name: "Wellampitiya", photos: [43,44,45] },
+        { name: "Moratuwa", photos: [49,50,51] }
         
       ]
     },
