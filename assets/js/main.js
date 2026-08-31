e42ac9168969b6fa80add79fd8fa6f8ab5e26ab1
supuni2006 <supuni2006k@gmail.com>
2026-08-31 16:40:44 +0530

    33

--- assets/js/main.js
+++ assets/js/main.js
@@ -178,7 +178,9 @@
         { name: "Project 02", photos: [14,16,17] },
         { name: "Project 03", photos: [46,47,48] },
         { name: "Wellampitiya", photos: [43,44,45] },
-        { name: "Moratuwa", photos: [49,50,51] }
+        { name: "Moratuwa", photos: [49,50,51] },
+        { name: "Microchip Solution - Moratuwa", photos: [55,56,57,58,59] }
+        
         
       ]
     },
